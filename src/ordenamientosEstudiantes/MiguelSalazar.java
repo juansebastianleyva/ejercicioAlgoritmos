a783fb07bf907def05a0b8556da35ab68c7cb7ec
Miguel Salazar <miguelsalazar88@gmail.com>
2023-03-24 17:40:28 -0500

    primer commit Miguel

---
+++ src/ordenamientosEstudiantes/MiguelSalazar.java
@@ -0,0 +1,19 @@
+package ordenamientosEstudiantes;
+
+public class MiguelSalazar implements JuanLeyva{
+    @Override
+    public String ordenarAsc(String[] numbers) {
+
+        // Se convierte el arreglo de strings a enteros
+        int[] arr = new int[numbers.length];
+        for (int i=0; i< numbers.length; i++){
+            arr[i] = Integer.parseInt(numbers[i]);
+        }
+        return null;
+    }
+
+    @Override
+    public String ordenarDesc(String[] numbers) {
+        return null;
+    }
+}
