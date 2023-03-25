50f4423c7c60a7b6bcb071ab4ae8df77c72b2f9d
Sala Golden <golden@academia.loc>
2023-03-24 22:51:36 -0500

    Agrego la nueva clase de ordenamiento, solo implementando la interfase de JuanLeyva

---
+++ src/ordenamientosEstudiantes/ordenamiento.java
@@ -0,0 +1,16 @@
+package ordenamientosEstudiantes;
+
+public class ordenamiento implements JuanLeyva{
+
+	@Override
+	public String ordenarAsc(String[] numbers) {
+		return null;
+	}
+
+	@Override
+	public String ordenarDesc(String[] numbers) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
