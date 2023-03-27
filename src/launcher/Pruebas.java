07e5b205bcfb9ff29920027fc220cc8fbfe8dd62
Sebaru18 <89612368+Sebaru18@users.noreply.github.com>
2023-03-26 19:59:05 -0500

    Cambio Pruebas

--- src/launcher/Pruebas.java
+++ src/launcher/Pruebas.java
@@ -6,15 +6,12 @@ public class Pruebas {
 	public static void main(String[]args) {
 		
 		//Cambiar el null por el constructor de su clase!
-		JuanLeyva jl=null;
+		JuanLeyva jl=new SebastianRuiz;
 		
 		String[] num= {"56","12","64","34","87","56"};
 		
 		System.out.println(jl.ordenarAsc(num));
 		System.out.println(jl.ordenarDesc(num));
-		
-		
-		
 	}
 
 }
